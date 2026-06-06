ab5e2ba586e7b50357bcab3d3a12f69b67d72422
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-06-06 18:36:36 -0400

    add clean function for removing puncuation and name differences

--- main.py
+++ main.py
@@ -6,28 +6,42 @@ def merge_csvs(recieving: pd.DataFrame, adp: pd.DataFrame):
     merged = pd.merge(
         recieving, adp, on=["player", "team_name", "position"], how="inner"
     )
-    merged.drop(columns=["Notes", "Id"])
+    merged = merged.drop(columns=["Notes", "Id"])
     merged.to_csv("merged.csv", index=False)
 
 
 def merge_finish(merged: pd.DataFrame, finish: pd.DataFrame):
     merged = merged.merge(
-        finish["fantasyPts", "ptsPerSnapptsPerTouch"],
+        finish[
+            [
+                "player",
+                "team_name",
+                "position",
+                "fantasyPts",
+                "ptsPerSnap",
+                "ptsPerTouch",
+            ]
+        ],
         on=["player", "team_name", "position"],
         how="left",
     )
     merged.to_csv("merged_points.csv", index=False)
 
 
+def clean_data(dataframes: list[pd.DataFrame]):
+    for df in dataframes:
+        df["player"] = df["player"].str.replace(" Jr.", "", regex=False)
+        df["player"] = df["player"].str.replace(".", "", regex=False)
+
+
 def main():
     recieving = pd.read_csv("pff_recieving/receiving_summary_2024.csv")
     adp = pd.read_csv("preseason_adp/2025ADP.csv")
-    recieving["player"] = recieving["player"].str.replace(" Jr.", "", regex=False)
-    adp["player"] = adp["player"].str.replace(" Jr.", "", regex=False)
-    recieving["player"] = recieving["player"].str.replace(".", "", regex=False)
-    adp["player"] = adp["player"].str.replace(".", "", regex=False)
-    # merge_csvs(recieving, adp)
-    # return
+    finish = pd.read_csv("points_finish/receiving_finish_2025.csv")
+    merged = pd.read_csv("merged.csv")
+    clean_data([recieving, adp, finish])
+    merge_finish(merged, finish)
+    return
     # merged = pd.read_csv("merged.csv")
     seen_pff = set()
     seen_underdog = set()
