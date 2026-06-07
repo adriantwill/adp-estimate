9fe8912290ba0c44a83137d4d82ac4f4b3ce9f85
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-06-07 17:59:48 -0400

    add basic linear regression with numpy

--- main.py
+++ main.py
@@ -1,59 +1,74 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import add_dummy_feature
+from sklearn.utils.validation import np
 
 
-def merge_csvs(recieving: pd.DataFrame, adp: pd.DataFrame):
-
-    merged = pd.merge(
-        recieving, adp, on=["player", "team_name", "position"], how="inner"
-    )
+def merge_csvs(recieving: pd.DataFrame, adp: pd.DataFrame, finish: pd.DataFrame):
+    merged = pd.merge(recieving, adp, on=["player", "position"], how="inner")
     merged = merged.drop(columns=["Notes", "Id"])
-    merged.to_csv("merged.csv", index=False)
-
-
-def merge_finish(merged: pd.DataFrame, finish: pd.DataFrame):
     merged = merged.merge(
         finish[
             [
                 "player",
-                "team_name",
                 "position",
                 "fantasyPts",
                 "ptsPerSnap",
                 "ptsPerTouch",
             ]
         ],
-        on=["player", "team_name", "position"],
-        how="left",
+        on=["player", "position"],
+        how="inner",
     )
-    merged.to_csv("merged_points.csv", index=False)
+    merged.to_csv("merged.csv", index=False)
 
 
 def clean_data(dataframes: list[pd.DataFrame]):
     for df in dataframes:
         df["player"] = df["player"].str.replace(" Jr.", "", regex=False)
         df["player"] = df["player"].str.replace(".", "", regex=False)
+        non_wr_rows = df.index[df["position"] != "WR"]
+        df.drop(index=non_wr_rows, inplace=True)
 
 
-def main():
-    recieving = pd.read_csv("pff_recieving/receiving_summary_2024.csv")
-    adp = pd.read_csv("preseason_adp/2025ADP.csv")
-    finish = pd.read_csv("points_finish/receiving_finish_2025.csv")
-    merged = pd.read_csv("merged.csv")
-    clean_data([recieving, adp, finish])
-    merge_finish(merged, finish)
-    return
-    # merged = pd.read_csv("merged.csv")
+def check_data(
+    recieving: pd.DataFrame,
+    adp: pd.DataFrame,
+    finish: pd.DataFrame,
+    merged: pd.DataFrame,
+):
     seen_pff = set()
     seen_underdog = set()
     dups = []
-    for row in adp.itertuples():
+    for row in merged.itertuples():
         seen_underdog.add(row.player)
     for row in recieving.itertuples():
+        if row.player in seen_pff:
+            dups.append(row.player)
         seen_pff.add(row.player)
-    for row in adp.itertuples():
-        if row.player not in seen_pff and row.position == "WR":
+    for row in recieving.itertuples():
+        if row.player not in seen_underdog and row.position == "WR":
             dups.append(row.player)
     print(dups)
+
+
+def main():
+    # recieving = pd.read_csv("pff_recieving/receiving_summary_2024.csv")
+    # adp = pd.read_csv("preseason_adp/2025ADP.csv")
+    # finish = pd.read_csv("points_finish/receiving_finish_2025.csv")
+    # clean_data([recieving, adp, finish])
+    # merge_csvs(recieving, adp, finish)
+    merged = pd.read_csv("merged.csv")
+    X = merged["ADP"].to_numpy()
+    X = X.reshape(-1, 1)
+    X_b = add_dummy_feature(X)
+    y = merged["fantasyPts"].to_numpy()
+    y = y.reshape(-1, 1)
+    print(y)
+    theta = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
+    plt.plot(X, y, "b.")
+    plt.show()
+    print(theta)
 
 
 if __name__ == "__main__":
