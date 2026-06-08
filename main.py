874040a61fe64259b0685d805c86db24bf01ef81
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-06-07 20:52:16 -0400

    add training on all relevant features from merged.csv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import add_dummy_feature
-from sklearn.utils.validation import np
 
 
 def merge_csvs(recieving: pd.DataFrame, adp: pd.DataFrame, finish: pd.DataFrame):
@@ -52,6 +53,21 @@ def check_data(
     print(dups)
 
 
+def numpy_linreg(X_b, y):
+    theta = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
+    mse = 0
+    for i in range(len(X_b)):
+        mse += (theta.T @ X_b[i] - y[i]) ** 2
+    mse /= len(X_b)
+    rmse = np.sqrt(mse)
+    print(rmse)
+
+
+def sklearn_linreg(X, y):
+    lin_reg = LinearRegression()
+    lin_reg.fit(X, y)
+
+
 def main():
     # recieving = pd.read_csv("pff_recieving/receiving_summary_2024.csv")
     # adp = pd.read_csv("preseason_adp/2025ADP.csv")
@@ -59,16 +75,28 @@ def main():
     # clean_data([recieving, adp, finish])
     # merge_csvs(recieving, adp, finish)
     merged = pd.read_csv("merged.csv")
-    X = merged["ADP"].to_numpy()
-    X = X.reshape(-1, 1)
+    nan_cols = merged.columns[merged.isna().any()]
+    print(nan_cols.tolist())
+    exclude = [
+        "player_id",
+        "declined_penalties",
+        "fantasyPts",
+        "ptsPerSnap",
+        "ptsPerTouch",
+        "franchise_id",
+    ]
+    exclude.extend(merged.columns[merged.isna().any()].to_list())
+    X_df = merged.drop(columns=exclude)
+    X_df = X_df.select_dtypes(include="number")
+    features = X_df.columns.to_list()
+    X = merged[features].to_numpy().reshape(-1, len(features))
     X_b = add_dummy_feature(X)
-    y = merged["fantasyPts"].to_numpy()
-    y = y.reshape(-1, 1)
+    y = merged["fantasyPts"].to_numpy().reshape(-1, 1)
     print(y)
-    theta = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
-    plt.plot(X, y, "b.")
-    plt.show()
-    print(theta)
+    numpy_linreg(X_b, y)
+    # sklearn_linreg(X, y)
+    # plt.plot(X, y, "b.")
+    # plt.show()
 
 
 if __name__ == "__main__":
