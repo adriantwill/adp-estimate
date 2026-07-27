04a0ebd9efa805fa7e58458c4a53ec19b7ad4a10
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-07-27 15:07:52 -0400

    add te and rb rows

--- src/proj_staps.py
+++ src/proj_staps.py
@@ -7,12 +7,7 @@ from util import normalize_player_name
 
 
 def main():
-    year = 2023
-    depth = nfl.load_depth_charts(seasons=year)
-    stats = nfl.load_player_stats(
-        seasons=year, summary_level="reg+post"
-    )  # maybe just reg
-    proj_wr_starter(depth, stats, "SEA", year)
+    proj_wr_starter("SEA", 2023)
 
 
 def player_stats(df: pl.DataFrame) -> Tuple[str, str]:
@@ -34,46 +29,65 @@ def player_stats(df: pl.DataFrame) -> Tuple[str, str]:
     return row
 
 
-def proj_wr_starter(depth: pl.DataFrame, stats: pl.DataFrame, team: str, year: int):
+def merge_pff_college(df: pl.DataFrame, year: int):
     college_stats = pl.read_csv(f"data/source/pff/pff_college/receiving_{year - 1}.csv")
-    input_stats = nfl.load_player_stats(
-        seasons=year - 1, summary_level="reg+post"
-    )  # mayb
-    wr_depth = depth.filter(
-        (pl.col("club_code") == team)
-        & (pl.col("position") == "WR")
-        # | (pl.col("position") == "TE")
-        # | (pl.col("position") == "RB")
-        & (pl.col("week") == 1)
-        # & (pl.col("season") == df["year"])
-    )
-    wr_depth = wr_depth.rename({"gsis_id": "player_id"})
-    wr_depth = wr_depth.unique(subset=["player_id"], keep="first")
-    y_wr_depth = wr_depth.join(stats, on="player_id")
-    x_wr_depth = wr_depth.select(["full_name", "player_id"]).join(
-        input_stats,
-        on=[
-            "player_id",
-        ],
-        how="left",
-    )
-    print(x_wr_depth["full_name"])
-    x_wr_depth = x_wr_depth.with_columns(
+    df = df.with_columns(
         pl.col("full_name").map_elements(normalize_player_name).alias("normalized_name")
     )
     college_stats = college_stats.with_columns(
         pl.col("player").map_elements(normalize_player_name).alias("normalized_name")
     )
-    x_wr_depth = x_wr_depth.join(college_stats, on=["normalized_name"], how="left")
-    x_wr_depth = x_wr_depth.filter(
-        pl.col("team_name").is_not_null() ^ pl.col("recent_team").is_not_null()
+    df = df.join(college_stats, on=["normalized_name"], how="left", coalesce=True)
+    # AVOID USING MAP_ELEMENTS
+    return df
+
+
+def proj_wr_starter(team: str, year: int):
+    depth = nfl.load_depth_charts(seasons=year)
+    output_stats = nfl.load_player_stats(
+        seasons=year, summary_level="reg+post"
+    )  # maybe just reg
+    output_stats = output_stats[["player_id", "target_share"]]
+    draft_stats = nfl.load_draft_picks(seasons=year)
+    draft_stats = draft_stats[["gsis_id", "pick"]]
+    draft_stats = draft_stats.with_columns(pl.lit(1).alias("is_rookie"))
+    input_stats = nfl.load_player_stats(seasons=year - 1, summary_level="reg+post")
+    wr_depth = depth.filter(
+        (pl.col("club_code") == team)
+        & (
+            (pl.col("position") == "WR")
+            | (pl.col("position") == "TE")
+            | (pl.col("position") == "RB")
+        )
+        & (pl.col("week") == 1)
+        # & (pl.col("season") == df["year"])
     )
-    print(
-        x_wr_depth[
-            ["player_id", "player_name", "recent_team", "full_name", "team_name"]
+    output_stats = output_stats.rename({"player_id": "gsis_id"})
+    input_stats = input_stats.rename({"player_id": "gsis_id"})
+    wr_depth = wr_depth.filter(
+        (pl.col("depth_position") == "WR")
+        | (pl.col("depth_position") == "TE")
+        | (pl.col("position") == "RB")
+    )
+    wr_depth = wr_depth.join(output_stats, on="gsis_id")
+    wr_depth = wr_depth.join(draft_stats, on="gsis_id", how="left")
+    wr_depth = wr_depth.join(input_stats, on="gsis_id", how="left")
+    # wr_depth = wr_depth.filter(
+    #     pl.col("recent_team").is_not_null() ^ pl.col("is_rookie") == 1
+    # )
+    wr_depth = wr_depth[
+        [
+            "full_name",
+            "pick",
+            "is_rookie",
+            "target_share",
+            "racr",
+            "receiving_epa",
+            "target_share_right",
         ]
-    )
-    y = y_wr_depth.select(["target_share", "full_name"])
+    ]
+    print(wr_depth)
+    print(sum(wr_depth["target_share"]))
 
 
 if __name__ == "__main__":
